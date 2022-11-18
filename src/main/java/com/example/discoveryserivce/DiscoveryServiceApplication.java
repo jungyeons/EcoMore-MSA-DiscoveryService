195f6317ea2d131c13dc9b4635f9b4d9036ab6fe
BaeJungYeon <qowjdduswkd@naver.com>
2022-11-18 23:57:51 +0900

    22.11.18

--- src/main/java/com/example/discoveryserivce/DiscoveryServiceApplication.java
+++ src/main/java/com/example/discoveryserivce/DiscoveryServiceApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class DiscoverySerivceApplication {
+public class DiscoveryServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DiscoverySerivceApplication.class, args);
+        SpringApplication.run(DiscoveryServiceApplication.class, args);
     }
 
 }
